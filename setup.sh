e0dc1b02a289188da5d891ec235ed00266a6abf3
Josh Bainbridge <josh.bainbridge@gmail.com>
2021-12-12 19:00:48 +0000

    Add new configs and extend setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-cd $HOME
-ln -s $OLDPWD/vim-config .vim
-ln -s $OLDPWD/tmux.conf .tmux.conf
+LINKS=(
+	".ssh/config shh-config"
+	".gitconfig git-config"
+	".tmux.conf tmux-config"
+	".vim vim-config"
+)
+
+MACOS_LINKS=(
+	".zshrc zshrc-config"
+)
+
+if [ ! -z "$1" ] && [ "$1" = "macos" ]; then
+	LINKS=("${LINKS[@]}" "${MACOS_LINKS[@]}")
+fi
+
+error() {
+	echo "Error: $1 already exists; please remove and run again" >&2
+	exit 2
+}
+
+for ELEMENT in "${LINKS[@]}"; do
+	PAIR=( $ELEMENT );
+	[ -e "${HOME}/${PAIR[0]}" ] && error "~/${PAIR[0]}"
+done
+
+for ELEMENT in "${LINKS[@]}"; do
+	PAIR=( $ELEMENT );
+	ln -s "${PWD}/${PAIR[1]}" "${HOME}/${PAIR[0]}"
+done
+
